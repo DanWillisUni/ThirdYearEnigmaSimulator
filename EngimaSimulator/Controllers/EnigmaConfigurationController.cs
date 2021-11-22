866d004c4caad8a7dd0cf0be35a1caeccacc1602
DanWillisUni <dan.willis.uni@gmail.com>
2021-11-22 13:06:09 +0000

    added logging in the controllers

--- EngimaSimulator/Controllers/EnigmaConfigurationController.cs
+++ EngimaSimulator/Controllers/EnigmaConfigurationController.cs
@@ -25,28 +25,37 @@ namespace EngimaSimulator.Controllers
             _logger = logger;
             _physicalConfiguration = physicalConfiguration;
             _basicConfiguration = basicConfiguration;
+            _logger.LogDebug("Physical Configuration: " + JsonConvert.SerializeObject(_physicalConfiguration));
+            _logger.LogDebug("Basic Configuration: " + JsonConvert.SerializeObject(_basicConfiguration));
         }
         public IActionResult Plugboard()
         {
+            _logger.LogInformation("Get Plugboard");
             EnigmaModel currentSave = EnigmaConfiguration.getCurrentSave(Path.Combine(_basicConfiguration.tempConfig.dir, _basicConfiguration.tempConfig.fileName));
+            _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(currentSave));
             PlugboardViewModel pvm = new PlugboardViewModel(currentSave.plugboard);
             return View(pvm);
         }
         public IActionResult Rotors()
         {
+            _logger.LogInformation("Get rotors");
             EnigmaModel currentSave = EnigmaConfiguration.getCurrentSave(Path.Combine(_basicConfiguration.tempConfig.dir, _basicConfiguration.tempConfig.fileName));
+            _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(currentSave));
             RotorViewModel rvm = new RotorViewModel(currentSave.rotors,_physicalConfiguration);
             return View(rvm);
         }
         [HttpPost]
         public IActionResult Rotors(RotorViewModel modelIn)
         {
+            _logger.LogInformation("Post rotors");
             RotorViewModel modelOut = new RotorViewModel();
             modelOut._physicalConfiguration = this._physicalConfiguration;
             EnigmaModel enigmaModel = new EnigmaModel();
             switch (modelIn.Command)
             {
                 case "rotorSave":
+                    _logger.LogInformation("Save the rotors");
+                    _logger.LogInformation("Saving rotors to: " + String.Join(", ", modelIn.liveRotorsNames.ToArray()));
                     foreach (string rn in modelIn.liveRotorsNames)
                     {
                         foreach (Rotor r in _physicalConfiguration.rotors)
@@ -63,8 +72,10 @@ namespace EngimaSimulator.Controllers
                     {
                         modelOut.liveRotorsNames.Add(r.rotor.name);
                     }
+                    _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(enigmaModel));
                     return View(modelOut);
                 case "rotorSaveOrder":
+                    _logger.LogInformation("ReOrder rotors");
                     //get new order
                     bool continueOn = true;
                     int counter = 1;
@@ -82,11 +93,13 @@ namespace EngimaSimulator.Controllers
                         }
                         counter++;
                     } while (continueOn);
+                    //get previous order
                     EnigmaModel currentSave = EnigmaConfiguration.getCurrentSave(Path.Combine(_basicConfiguration.tempConfig.dir, _basicConfiguration.tempConfig.fileName));
                     foreach (RotorModel r in currentSave.rotors)
                     {
                         modelOut.liveRotorsNames.Add(r.rotor.name);
                     }
+                    _logger.LogInformation("Previous Rotor order: " + String.Join(", ", modelOut.liveRotorsNames.ToArray()));
                     //order swap
                     List<string> tempRotors = new List<string>();
                     foreach(int i in newRotorOrder)
@@ -94,7 +107,7 @@ namespace EngimaSimulator.Controllers
                         tempRotors.Add(modelOut.liveRotorsNames[i - 1]);
                     }
                     modelOut.liveRotorsNames = tempRotors;
-
+                    _logger.LogInformation("New Rotor order: " + String.Join(", ", modelOut.liveRotorsNames.ToArray()));
                     foreach (string rn in modelOut.liveRotorsNames)
                     {
                         foreach (Rotor r in _physicalConfiguration.rotors)
@@ -111,13 +124,16 @@ namespace EngimaSimulator.Controllers
                     {
                         modelOut.liveRotorsNames.Add(r.rotor.name);
                     }
+                    _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(enigmaModel));
                     return View(modelOut);
-                case "Enigma":                    
+                case "Enigma":
+                    _logger.LogInformation("Go to the simulator from rotors");
                     enigmaModel = EnigmaConfiguration.getCurrentSave(Path.Combine(_basicConfiguration.tempConfig.dir, _basicConfiguration.tempConfig.fileName));
-                    foreach (RotorModel r in enigmaModel.rotors)
+                    /*foreach (RotorModel r in enigmaModel.rotors)
                     {
                         modelOut.liveRotorsNames.Add(r.rotor.name);
-                    }
+                    }*/
+                    _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(enigmaModel));
                     MainViewModel mainviewmodel = new MainViewModel(enigmaModel);
                     return View("../Enigma/Index", mainviewmodel);
                 default:
@@ -128,13 +144,16 @@ namespace EngimaSimulator.Controllers
 
         public IActionResult Reflector()
         {
+            _logger.LogInformation("Get reflector");
             EnigmaModel currentSave = EnigmaConfiguration.getCurrentSave(Path.Combine(_basicConfiguration.tempConfig.dir, _basicConfiguration.tempConfig.fileName));
+            _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(currentSave));
             ReflectorViewModel rvm = new ReflectorViewModel(currentSave.reflector, _physicalConfiguration);
             return View(rvm);
         }
         [HttpPost]
         public IActionResult Reflector(ReflectorViewModel modelIn)
         {
+            _logger.LogInformation("Post reflector");
             ReflectorViewModel modelOut = new ReflectorViewModel();
             modelOut._physicalConfiguration = this._physicalConfiguration;
             EnigmaModel enigmaModel = new EnigmaModel();
@@ -142,15 +161,18 @@ namespace EngimaSimulator.Controllers
             {
                 if (r.name == modelIn.liveReflectorName)
                 {
+                    _logger.LogInformation("New reflector: " + modelIn.liveReflectorName);
                     enigmaModel.reflector = new RotorModel(r);
                     break;
                 }
             }
             EnigmaModel mergedEnigmaModel = EnigmaConfiguration.mergeEnigmaConfiguration(enigmaModel, Path.Combine(_basicConfiguration.tempConfig.dir, _basicConfiguration.tempConfig.fileName));
             modelOut.liveReflectorName = mergedEnigmaModel.reflector.rotor.name;
+            _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(mergedEnigmaModel));
             switch (modelIn.Command)
             {              
                 case "Enigma":
+                    _logger.LogInformation("Go to simulator from Reflector");
                     MainViewModel mainviewmodel = new MainViewModel(mergedEnigmaModel);
                     return View("../Enigma/Index", mainviewmodel);
                 default:
--- EngimaSimulator/Controllers/EnigmaController.cs
+++ EngimaSimulator/Controllers/EnigmaController.cs
@@ -28,51 +28,69 @@ namespace EngimaSimulator.Controllers
         }        
         public IActionResult Index()
         {
+            _logger.LogInformation("Load main page");
             EnigmaModel enigmaModel = EnigmaConfiguration.getCurrentSave(Path.Combine(_basicConfiguration.tempConfig.dir, _basicConfiguration.tempConfig.fileName));
+            _logger.LogInformation("Current save: " + JsonConvert.SerializeObject(enigmaModel));
             MainViewModel model = new MainViewModel(enigmaModel);
             return View(model);
         }
         [HttpPost]
         public IActionResult Index(MainViewModel modelIn)
-        {            
+        {
+            _logger.LogInformation("Post index");
             return View(modelIn);
         }
 
         #region encoding
         private char encode(EnigmaModel em,char input)
         {
+            _logger.LogDebug("Encode: " + input);
             char current = input;
             current = plugboardSwap(em.plugboard,current);
+            _logger.LogDebug("Encoding rotors right to left");
             foreach (RotorModel r in em.rotors.Reverse<RotorModel>())
             {
                 current = rotorEncode(r,current);
             }
+            _logger.LogDebug("Reflector");
             current = rotorEncode(em.reflector, current);
+            _logger.LogDebug("Encoding rotors left to right");
             foreach (RotorModel r in em.rotors)
             {
                 current = rotorEncode(r, current);
             }
             current = plugboardSwap(em.plugboard,current);
+            _logger.LogInformation(input + ":" + current);
             return current;
         }
         private char plugboardSwap(Dictionary<char,char> plugboard,char input)
         {
+            _logger.LogDebug("plugboardSwap: " + input);
             foreach (KeyValuePair<char, char> entry in plugboard)
             {
                 if (entry.Key.Equals(input))
                 {
+                    _logger.LogDebug("Swapped " + input + " with " + entry.Value);
                     return entry.Value;
                 }
                 if (entry.Value.Equals(input))
                 {
+                    _logger.LogDebug("Swapped " + input + " with " + entry.Key);
                     return entry.Key;
                 }
             }
+            _logger.LogDebug("No swap was made");
             return input;
         }
         private char rotorEncode(RotorModel rm,char input)
-        {
-            return rm.rotor.order[(Convert.ToInt32(input) - 65 + rm.rotation) % 26];
+        {           
+            int charNumber = Convert.ToInt32(input) - 65;
+            _logger.LogDebug("Input: " + input + "/" + charNumber);
+            _logger.LogDebug("Rotor rotation: " + rm.rotation);
+            _logger.LogDebug("Order: " + rm.rotor.order);
+            char r = rm.rotor.order[(charNumber + rm.rotation) % 26];
+            _logger.LogDebug("Returns " + r);
+            return r;
         }
         #endregion
 
