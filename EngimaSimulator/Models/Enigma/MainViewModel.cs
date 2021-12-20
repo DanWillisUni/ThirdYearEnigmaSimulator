156af4f1219a2056b03911be9dabf2db1e15d263
DanWillisUni <dan.willis.uni@gmail.com>
2021-12-20 12:34:11 +0000

    plugboard view

--- EngimaSimulator/Models/Enigma/MainViewModel.cs
+++ EngimaSimulator/Models/Enigma/MainViewModel.cs
@@ -13,6 +13,16 @@ namespace EngimaSimulator.Models.Enigma
         public string Command { get; set; }
         public string outputTextBox { get; set; }
         public string inputTextBox { get; set; }
+        public string plugboardPairs { get {
+                string r = "";
+                foreach(KeyValuePair<int, int> entry in enigmaModel.plugboard)
+                {
+                    r += Convert.ToChar(entry.Key + 65);
+                    r += Convert.ToChar(entry.Value + 65);
+                    r += " ";
+                }
+                return r;
+            } }
         public MainViewModel()
         {
             this.enigmaModel = new EnigmaModel();
